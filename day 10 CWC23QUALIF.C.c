08dc02eec3d5b7d89bc034df19f279e0b3c38b08
Adityathakur2004 <adityachauhan178@gmail.com>
2024-10-07 20:23:20 +0530

    Add files via upload

---
+++ day 10 CWC23QUALIF.C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T); // Read the number of test cases
+
+    while (T--) {
+        int W, X, Y, Z;
+        scanf("%d %d %d %d", &W, &X, &Y, &Z); // Read W, X, Y, Z
+
+        // Check combinations
+        if (W == X || W == Y || W == Z || 
+            W == (X + Y) || W == (X + Z) || W == (Y + Z) || 
+            W == (X + Y + Z)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
+
+
