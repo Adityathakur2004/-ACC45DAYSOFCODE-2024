26f7f99cac46a01e640f2a2d595031cad94a25c3
Adityathakur2004 <adityachauhan178@gmail.com>
2024-11-11 21:46:49 +0530

    Add files via upload

---
+++ day 40cwc23qualif.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	    int x,y,a,b;
+	    scanf("%d %d",&x,&y);
+	    a=y/2;
+	    b=x-y;
+	    printf("%d\n",a+b);
+	   
+	return 0;
+}
+
